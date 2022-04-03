149d43988ba35e0f6a1a14548a716469d0288244
Ty Maraist <tcmaraist@gmail.com>
2022-04-03 18:41:21 -0500

    rename api.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import api from "../utils/Api";
+import api from "../utils/api";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
